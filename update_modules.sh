a603b9d57e6f376560406467f7ba30939475f6f7
Jummit <jummit@web.de>
2021-01-18 18:02:22 +0100

    initial commit

---
+++ update_modules.sh
@@ -0,0 +1,35 @@
+#!bin/sh
+indent=""
+arg=$1
+
+rm -rf repos
+mkdir -p repos
+
+download() {
+	while read url; do
+		name=$(basename ${url%.git})
+		destination="repos/$name"
+		echo "${indent}[$name] from $url"
+		git clone -q $url $destination
+		cd ../addons
+		for addon in ../package_manager/repos/${name}/addons/*; do
+			rm -rf "$(basename "$addon")"
+			if [ "$arg" = "--symlinks" ]; then
+				ln -sr ../package_manager/repos/${name}/addons/* .
+			else
+				cp -r "../package_manager/repos/${name}/addons/$(basename "$addon")" .
+			fi
+		done
+		cd ../package_manager
+		if [ -e "$destination/.godotmodules" ]; then
+			indent="${indent}\t"
+			download "${destination}/.godotmodules"
+			indent=$(echo $indent | sed "s/^.//")
+		fi
+	done < $1
+}
+download ../.godotmodules
+
+if [ "$1" != "--symlinks" ]; then
+	rm -rf repos
+fi
